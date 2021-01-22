67d032ea26dd372f2c217324b4f27dc4489bc747
Alan Renner <renner.131@osu.edu>
2021-01-21 19:53:04 -0800

    scripts

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const mongoose = require("mongoose");
+const BodyParser = require("body-parser");
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const PORT = process.env.PORT || 5000;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+const app = express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var mongoDB = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connection successful"));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB Connection Error"));
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(express.static(path.join(__dirname, "/client/build")));
+let fileLoc = "";
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+if (process.env.NODE_ENV === "production")
+  fileLoc = path.join(__dirname + "/client/public/index.html");
+else fileLoc = path.join(__dirname + "/client/public/index.html");
 
-module.exports = app;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
